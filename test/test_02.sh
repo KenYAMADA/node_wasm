0cfaeaae077be806eff69b5cfdc89dbb34210379
masashi.g <masashi.g@gmail.com>
2019-01-19 10:44:33 +0900

    binary operator (+, -, *, /, %)

---
+++ test/test_02.sh
@@ -0,0 +1,3 @@
+sh test_exitcode.sh mininode_wasm_02.js mininode_14.js add.js
+sh test_exitcode.sh mininode_wasm_02.js mininode_14.js add_many.js
+sh test_exitcode.sh mininode_wasm_02.js mininode_14.js binoperator.js
