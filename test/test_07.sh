2a2418f1704aeaa79c0f52c940c2c2255af242af
masashi.g <masashi.g@gmail.com>
2019-01-19 11:31:48 +0900

    step7: user defined functijon

---
+++ test/test_07.sh
@@ -0,0 +1,4 @@
+sh test_stdout.sh mininode_wasm_07.js mininode_14.js func_add.js
+sh test_stdout.sh mininode_wasm_07.js mininode_14.js fizzbuzz_func.js
+sh test_stdout.sh mininode_wasm_07.js mininode_14.js fib_func.js
+
