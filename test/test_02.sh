3779ae38d80fdec4e63727a76cac236797b0097d
masashi.g <masashi.g@gmail.com>
2019-03-09 18:47:50 +0900

    step 2, binary operator

--- test/test_02.sh
+++ test/test_02.sh
@@ -1,3 +1,5 @@
+sh test_exitcode.sh mininode_wasm_02.js mininode_14.js eight.js
+
 sh test_exitcode.sh mininode_wasm_02.js mininode_14.js add.js
 sh test_exitcode.sh mininode_wasm_02.js mininode_14.js add_many.js
 sh test_exitcode.sh mininode_wasm_02.js mininode_14.js binoperator.js
